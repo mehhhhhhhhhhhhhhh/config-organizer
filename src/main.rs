111825c6830e123fce5c7ce5be8ff7d9737f2996
mehh <137650446+mehhhhhhhhhhhhhhh@users.noreply.github.com>
2023-05-16 17:27:43 +0100

    Cache def loading

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,32 @@ mod variable_definitions;
 use environment_definitions::EnvironmentDefinitions;
 
 use std::{env, io};
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 use std::fs::File;
 use std::path::{Path, PathBuf};
 use clap::Parser;
+use crate::variable_definitions::VariableSource;
 
 #[derive(Parser)]
 struct Args {
     #[arg()]
     directory: PathBuf,
+}
+
+struct VarDefParseCache {
+    cache: HashMap<PathBuf, VariableSource>,
+}
+impl VarDefParseCache {
+    fn load(&mut self, path: &Path) -> io::Result<&VariableSource> {
+        match self.cache.entry(path.to_path_buf()) {
+            Entry::Occupied(v) => Ok(v.into_mut()),
+            Entry::Vacant(v) => {
+                eprintln!("        loading!...");
+                Ok(v.insert(variable_definitions::load(path)?))
+            }
+        }
+    }
 }
 
 fn main() -> io::Result<()> {
@@ -22,12 +40,14 @@ fn main() -> io::Result<()> {
     let env_defs : EnvironmentDefinitions = serde_yaml::from_reader(envs_file).unwrap();
     let envs = env_defs.environments;
 
+    let mut cache = VarDefParseCache { cache: Default::default() };
+
     for (name, def) in envs {
         println!("{}:\n  {:?}", &name, &def);
         for var_source in def.configuration.variables {
             println!("    {}", &var_source);
             let path = format!("configuration/variables/{}.yml", &var_source);
-            let shit = variable_definitions::load(Path::new(&path));
+            let shit = cache.load(Path::new(&path));
             eprintln!("      {:?}", shit);
         }
     }
