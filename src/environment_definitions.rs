d7d2a5231a6cd873f22185a9624002e6705d34d1
mehh <137650446+mehhhhhhhhhhhhhhh@users.noreply.github.com>
2023-05-16 16:16:07 +0100

    Initial environments.yml parsing

---
+++ src/environment_definitions.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+use serde::{Deserialize};
+
+#[derive(Deserialize, Debug)]
+pub(crate) struct EnvironmentDefinitions {
+    pub environments: HashMap<String, EnvDef>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all="lowercase")]
+enum FrontendType {
+    Separate,
+    Integrated,
+}
+
+fn separatev() -> FrontendType { FrontendType::Separate }
+fn falsev() -> bool {false}
+
+#[derive(Deserialize, Debug)]
+pub(crate) struct EnvDef {
+    #[serde(default="separatev")]
+    frontend: FrontendType,
+    #[serde(default="falsev")]
+    reduced_memory: bool,
+    #[serde(default="falsev")]
+    debug_ports: bool,
+    configuration: ConfDef,
+    mocks: Vec<String>,
+}
+
+#[derive(Deserialize, Debug)]
+pub(crate) struct ConfDef {
+    variables: Vec<String>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+mod environment_definitions;
+
+use environment_definitions::EnvironmentDefinitions;
+
+use std::{env, io};
+use std::fs::File;
+use std::path::{Path, PathBuf};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg()]
+    directory: PathBuf,
+}
+
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+    env::set_current_dir(args.directory)?;
+
+    let envs_file = File::open("environments.yml")?;
+    let env_defs : EnvironmentDefinitions = serde_yaml::from_reader(envs_file).unwrap();
+    let envs = env_defs.environments;
+
+    for (name, def) in envs {
+        println!("{}:\n  {:?}", &name, &def);
+    }
+    Ok(())
 }
