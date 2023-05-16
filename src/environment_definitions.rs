0a153408fef56ac704651ad6bd8447bdd33df312
mehh <137650446+mehhhhhhhhhhhhhhh@users.noreply.github.com>
2023-05-16 17:32:54 +0100

    Drop unused fields

--- src/environment_definitions.rs
+++ src/environment_definitions.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
-use serde::{Deserialize};
+use serde::{
+    Deserialize,
+};
 
 #[derive(Deserialize, Debug)]
 pub(crate) struct EnvironmentDefinitions {
@@ -7,23 +9,7 @@ pub(crate) struct EnvironmentDefinitions {
 }
 
 #[derive(Deserialize, Debug)]
-#[serde(rename_all="lowercase")]
-enum FrontendType {
-    Separate,
-    Integrated,
-}
-
-fn separatev() -> FrontendType { FrontendType::Separate }
-fn falsev() -> bool {false}
-
-#[derive(Deserialize, Debug)]
 pub(crate) struct EnvDef {
-    #[serde(default="separatev")]
-    frontend: FrontendType,
-    #[serde(default="falsev")]
-    reduced_memory: bool,
-    #[serde(default="falsev")]
-    debug_ports: bool,
     pub configuration: ConfDef,
     mocks: Vec<String>,
 }
